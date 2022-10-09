afbe38a1ee502d8d285d4829dfd7e0ba33f4e9e3
Tomas Bjerre <tomas.bjerre85@gmail.com>
2022-10-09 08:19:01 +0200

    fix: updating release script

--- release.sh
+++ release.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-gpg -o /tmp/dummy --sign .gitignore \
- && nextVersion=$(npx git-changelog-command-line --print-next-version) \
- && ./mvnw versions:set -DnewVersion=$nextVersion-SNAPSHOT \
- && git commit -a -m "chore: setting version $nextVersion-SNAPSHOT"
+gpg -o /tmp/dummy --sign .gitignore
 
-./mvnw release:prepare release:perform -B -DperformRelease=true \
+./mvnw \
+  se.bjurr.gitchangelog:git-changelog-maven-plugin:1.95.0:semantic-version \
+  release:prepare \
+  release:perform \
+  -B \
+  -DperformRelease=true \
  && npx git-changelog-command-line -of CHANGELOG.md -ip ".*maven-release-plugin.*" \
  && git commit -a -m "chore: updating changelog" \
  && git push \
